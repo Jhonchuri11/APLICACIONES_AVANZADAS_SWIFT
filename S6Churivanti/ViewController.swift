afc8efafec1d4eddddeda126c3ff00dfdb8434be
Jhonchuri11 <jhon.churivanti@tecsup.edu.pe>
2024-04-20 10:07:36 -0500

    Initial Commit

---
+++ S6Churivanti/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  S6Churivanti
+//
+//  Created by Mac19 on 20/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
