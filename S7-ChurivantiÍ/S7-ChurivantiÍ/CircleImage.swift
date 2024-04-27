2385dd35d287dab364a9bbf782c3eee8c66d7a7a
Jhonchuri11 <jhon.churivanti@tecsup.edu.pe>
2024-04-27 10:54:25 -0500

    Semana06, desarrollo de laboratorio, scrollView, TableViewCell

---
+++ S7-ChurivantiÍ/S7-ChurivantiÍ/CircleImage.swift
@@ -0,0 +1,18 @@
+//
+//  CircleImage.swift
+//  S7-ChurivantiÍ
+//
+//  Created by Mac19 on 27/04/24.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleImage()
+}
---
+++ S7-ChurivantiÍ/S7-ChurivantiÍ/ImageCircle.swift
@@ -0,0 +1,18 @@
+//
+//  ImageCircle.swift
+//  S7-ChurivantiÍ
+//
+//  Created by Mac19 on 27/04/24.
+//
+
+import SwiftUI
+
+struct ImageCircle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImageCircle()
+}
